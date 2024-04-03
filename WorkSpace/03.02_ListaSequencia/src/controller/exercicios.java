f472a426a2c07d3c44d261667b0d3837b154b076
Rodrigo <144171797+Rodrigo5431@users.noreply.github.com>
2024-04-03 16:23:58 -0300

    Add files via upload

---
+++ WorkSpace/03.02_ListaSequencia/src/controller/exercicios.java
@@ -0,0 +1,11 @@
+package controller;
+
+public class exercicios {
+
+	public static void main(String[] args) {
+		int valor = Teclado.LerInt("Digite um número");
+		
+		
+		
+	}
+}
--- WorkSpace/03.1_ListaSequencia/src/controller/exercicios.java
+++ WorkSpace/03.1_ListaSequencia/src/controller/exercicios.java
@@ -3,8 +3,9 @@ import util.Teclado;
 
 public class exercicios {
 public static void main(String[] args) {
+	
 	String nome, sexo;
 	
-	nome = Teclado.
+	nome = Teclado.;
 }
 }
